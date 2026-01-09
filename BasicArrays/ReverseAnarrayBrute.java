96ec1556447ef66ee89df75467be5d960f72e68e
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-09 22:13:58 +0530

    Array reversal optimized

--- BasicArrays/ReverseAnarrayBrute.java
+++ BasicArrays/ReverseAnarrayBrute.java
@@ -1,11 +1,23 @@
 public class ReverseAnarrayBrute {
     public void Reverse(int arr[], int n) {
-        int temp[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            temp[i] = arr[n-i-1];
-        }
-        for (int i = 0; i < n; i++) {
-            arr[i] = temp[i];
+        //This method is not optimized as TC is O(N^2)
+
+        // int temp[] = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     temp[i] = arr[n-i-1];
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     arr[i] = temp[i];
+        // }
+
+        int p1 = 0;
+        int p2 = n - 1;
+        while (p1 < p2) {
+            int temp = arr[p1];
+            arr[p1] = arr[p2];
+            arr[p2] = temp;
+            p1++;
+            p2--;
         }
         return;
     }
