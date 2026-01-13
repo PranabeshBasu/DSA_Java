ff22a6fcfd9db299dbeb834e67d489e973d21261
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-13 21:50:03 +0530

    Updated

---
+++ Strings/Palindrome.java
@@ -0,0 +1,10 @@
+public class Palindrome {
+    public boolean isPalindrome(String s){
+        //code here
+    }
+    public static void main(String[] args) {
+        String s = "HAAAAH";
+        Palindrome p = new Palindrome();
+        System.out.println(p.isPalindrome(s));
+    }
+}
