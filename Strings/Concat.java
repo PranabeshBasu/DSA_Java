73f39c72675d7b46b7d9747e625f782664672a2b
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-17 20:19:12 +0530

    Operator overloading

---
+++ Strings/Concat.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Concat {
+    public static void main(String[] args) {
+        System.out.println('a'+'b'); // THESE CHARACTERS ARE ONLY TAKING THE ASCII VALUES
+        System.out.println("a" + "b"); //HERE TWO STRING OBJECTS ARE ADDED TO CREATE A NEW OBJ
+        // System.out.println(new Integer(56)+""); 
+        System.out.println("Pranabesh" + new ArrayList<>());
+    }
+}
