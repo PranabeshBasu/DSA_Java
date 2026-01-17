77cc1dd6b0476f2dca1d3bb0f5e8d7411c8dff29
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-17 17:52:43 +0530

    Palindrome String check

--- Strings/Palindrome.java
+++ Strings/Palindrome.java
@@ -1,6 +1,16 @@
 public class Palindrome {
     public boolean isPalindrome(String s){
         //code here
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
     public static void main(String[] args) {
         String s = "HAAAAH";
