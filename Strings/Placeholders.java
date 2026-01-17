b6e0c14bae8f01daab2c2c46b0b23efb64fa670d
PranabeshBasu <pranabeshbasu9@gmail.com>
2026-01-17 19:01:13 +0530

    Placeholders in java

---
+++ Strings/Placeholders.java
@@ -0,0 +1,8 @@
+public class Placeholders {
+    public static void main(String[] args) {
+        float f = 12.3455677755f;
+        System.out.printf("The value of f is %.2f",f);
+        System.out.printf("%n");
+        System.out.printf("The first student is %s and the second one is %s","Pranabesh","Pritam");
+    }
+}
